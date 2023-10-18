2942172f82e2c5ad9809a8ccf9a52e89e90bbd56
Ben Tideswell <ben@fishpig.co.uk>
2023-10-18 10:04:30 +0100

    1.0.0

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package FishPig_CssMediaQueryDebugger
+ * @author  Ben Tideswell (ben@fishpig.com)
+ * @url     https://fishpig.com/
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FishPig_CssMediaQueryDebugger', __DIR__);
